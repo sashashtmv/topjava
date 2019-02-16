9885a429be3e08cd50aba19f9a2ec893236886df
Alexandr Shtomov <sashashtmv06@gmail.com>
2019-02-16 22:06:41 +0200

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,13 +18,26 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> exceeds = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
+        exceeds.forEach(System.out::println);
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<LocalDateTime, Integer> callories = new HashMap<>();
+        for(UserMeal temp : mealList){
+            callories.put(temp.getDateTime(), callories.getOrDefault(temp.getDateTime(),0) + temp.getCalories());
+        }
+
+        List<UserMealWithExceed> result = new ArrayList<>();
+        for(UserMeal temp : mealList){
+            if(temp.getDateTime().toLocalTime().isAfter(startTime) && temp.getDateTime().toLocalTime().isBefore(endTime)){
+                result.add(new UserMealWithExceed(temp.getDateTime(), temp.getDescription(), temp.getCalories(),
+                        callories.get(temp.getDateTime()) > caloriesPerDay));
+            }
+        }
         // TODO return filtered list with correctly exceeded field
-        return null;
+        return result;
     }
 }
